832488153836cf80a62d37d9446269188a78c936
nivetha11n <49003463+nivetha11n@users.noreply.github.com>
2024-02-28 11:56:15 +0100

    testing the liveview page
    esting the liveview page

--- lib/liveviewtest_web/live/page_live.ex
+++ lib/liveviewtest_web/live/page_live.ex
@@ -2,15 +2,19 @@ defmodule LiveviewtestWeb.PageLive do
 
   use LiveviewtestWeb, :live_view
 
-    def mount(_params, _session, socket) do
-        {:ok, socket}
-    end
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, number: 5)}
+  end
 
-    def render(assigns) do
-     ~H"""
+  def render(assigns) do
+    ~H"""
+    <%= @number %>
+    <.button phx-click="add">Add</.button>
+    """
+  end
 
-     <strong>Hello World!</strong>
-     """
-    end
+  def handle_event("add", _params, socket) do
+    {:noreply, assign(socket, number: socket.assigns.number + 1 )}
+  end
 
 end
